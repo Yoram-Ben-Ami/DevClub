5fee45f25e9750415c68fcfe868919d1d3f8d691
Yoram-Ben-Ami <77965127+Yoram-Ben-Ami@users.noreply.github.com>
2022-05-23 11:13:58 +0300

    Delete my-app directory

--- my-app/src/compomes/AddFav.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AddFav = () => {
-	return (
-			<span className='mr-2'>Add to Favourites/ DELETE</span>
-	);
-};
-
-export default AddFav;
--- my-app/src/compomes/Header.js
+++
@@ -1,8 +0,0 @@
-function Header(props){
-    return(
-      <div className='col d-flex justify-content-center'>
-     <h1 class="display-1"><b>{props.heading}</b></h1>
-      </div>
-  )
-}
-export default Header;
